a1585cb02bff04bb642a48e7702de7f513109b4d
SimpPoseidon <120441879+TanishqMehrunkarIIPSDAVV@users.noreply.github.com>
2025-03-13 17:08:06 +0530

    commit for branch

--- src/App/Views/Components/Head.component.php
+++ src/App/Views/Components/Head.component.php
@@ -4,34 +4,34 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>To Do Website</title>
+    <title>Library Management</title>
     <script src="https://cdn.tailwindcss.com"></script>
-    <?=loadComponent("Tailwind")?>
+    <?= loadComponent("Tailwind") ?>
     <link rel="stylesheet" href="/css/style.css">
     <script>
         document.addEventListener('DOMContentLoaded', function() {
-    const themeToggle = document.getElementById('theme-toggle');
-    const html = document.documentElement;
+            const themeToggle = document.getElementById('theme-toggle');
+            const html = document.documentElement;
 
-    const currentTheme = localStorage.getItem('theme') || 'light';
-    if (currentTheme === 'dark') {
-        html.classList.add('dark');
-        themeToggle.checked = true;
-    } else {
-        html.classList.remove('dark');
-        themeToggle.checked = false;
-    }
+            const currentTheme = localStorage.getItem('theme') || 'light';
+            if (currentTheme === 'dark') {
+                html.classList.add('dark');
+                themeToggle.checked = true;
+            } else {
+                html.classList.remove('dark');
+                themeToggle.checked = false;
+            }
 
-    themeToggle.addEventListener('change', function() {
-        if (themeToggle.checked) {
-            html.classList.add('dark');
-            localStorage.setItem('theme', 'dark');
-        } else {
-            html.classList.remove('dark');
-            localStorage.setItem('theme', 'light');
-        }
-    });
-});
+            themeToggle.addEventListener('change', function() {
+                if (themeToggle.checked) {
+                    html.classList.add('dark');
+                    localStorage.setItem('theme', 'dark');
+                } else {
+                    html.classList.remove('dark');
+                    localStorage.setItem('theme', 'light');
+                }
+            });
+        });
     </script>
 
 </head>
